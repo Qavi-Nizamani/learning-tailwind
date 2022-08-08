82b0d111f76ddbbd9184798ebfbd5d891271d7e6
Qavi-Nizamani <Qaviniz11@gmail.com>
2022-08-08 17:37:07 +0500

    added navbar with font-awesome

---
+++ components/Navbar.js
@@ -0,0 +1,95 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Link from 'next/link';
+import { faBars, faClose } from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useState } from 'react';
+
+const Navbar = () => {
+  const [showNav, setShowNav] = useState(false);
+  const [color, setColor] = useState('transparent');
+  const [textColor, setTextColor] = useState('white');
+
+  const toggleNav = () => {
+    setShowNav(!showNav);
+  };
+
+  useEffect(() => {
+    const func = () => {
+      if (window.scrollY > 0) {
+        setColor('white');
+        setTextColor('black');
+      } else {
+        setColor('transparent');
+        setTextColor('white');
+      }
+    };
+    window.addEventListener('scroll', func);
+    return () => {
+      window.removeEventListener('scroll', func);
+    };
+  }, []);
+  return (
+    <div
+      style={{ backgroundColor: color }}
+      className="fixed left-0 top-0 w-full z-10 ease-in duration-300"
+    >
+      <div className="max-w-[1240px] m-auto flex justify-between items-center p-4 text-white">
+        <Link href="/">
+          <h1 style={{ color: textColor }} className="font-bold text-4xl">
+            Capture
+          </h1>
+        </Link>
+        <ul style={{ color: textColor }} className="hidden sm:flex">
+          <li className="p-4 ">
+            <Link href="/">Home</Link>
+          </li>
+          <li className="p-4 ">
+            <Link href="/#gallery">Gallery</Link>
+          </li>
+          <li className="p-4 ">
+            <Link href="/portfolio">Home</Link>
+          </li>
+          <li className="p-4 ">
+            <Link href="/contact">Home</Link>
+          </li>
+        </ul>
+
+        {/* Mobile Menu */}
+        <div
+          style={{ color: !showNav && textColor }}
+          className="block sm:hidden z-10 cursor-pointer"
+          onClick={toggleNav}
+        >
+          {showNav ? (
+            <FontAwesomeIcon icon={faClose} size="2x" />
+          ) : (
+            <FontAwesomeIcon icon={faBars} size="2x" />
+          )}
+        </div>
+        <div
+          className={
+            showNav
+              ? 'sm:hidden absolute top-0 bottom-0 left-0 right-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-200'
+              : 'sm:hidden absolute top-0 bottom-0 left-[-100%] right-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-200'
+          }
+        >
+          <ul>
+            <li className="p-4 text-4xl hover:text-gray-500">
+              <Link href="/">Home</Link>
+            </li>
+            <li className="p-4 text-4xl hover:text-gray-500">
+              <Link href="/#gallery">Gallery</Link>
+            </li>
+            <li className="p-4 text-4xl hover:text-gray-500">
+              <Link href="/portfolio">Home</Link>
+            </li>
+            <li className="p-4 text-4xl hover:text-gray-500">
+              <Link href="/contact">Home</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+import Navbar from '../components/Navbar';
+import '../styles/globals.css';
+import '@fortawesome/fontawesome-svg-core/styles.css'; // import Font Awesome CSS
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
