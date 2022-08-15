a82d40d26af881c8e3f2d38b4ccafad40bf52ced
Qavi-Nizamani <Qaviniz11@gmail.com>
2022-08-15 21:39:12 +0500

    Titles changed

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Hero
-        heading="Cature Photography"
+        heading="Qavi Photography"
         message="I capture moments in nature and keep them alive."
       />
       <Slider slides={sliderData} />
